ba15988a52be981228c517841527b729d66ecc24
Navoda Perera <navoda1@gmail.com>
2022-04-20 15:49:39 -0400

    Added API call to server from client

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,27 @@ import React from "react";
 
 import "./App.css";
 
+const handleFetch = () => {
+    fetch("/api", {
+        method: "GET",
+    })
+    .then((response) => {
+        return response.text()
+    })
+    .then((text) => {
+        console.log(text)
+    })
+}
+
 function App() {
     return(
-        <div className="App">
-            <h1> Hello, World!! </h1>
+        <div>
+            <div className="App">
+                <h1> Hello, World!! </h1>
+            </div>
+            <button onClick={handleFetch}>
+                API Call
+            </button>
         </div>
     );
 }
